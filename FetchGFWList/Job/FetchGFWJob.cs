487075a39ca46edb306e41cba55d1edce7393701
Abel Liu <abel.liu.china@outlook.com>
2016-12-28 15:06:33 +0800

    update

---
+++ FetchGFWList/Job/FetchGFWJob.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Quartz;
+using Quartz.Impl;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace FetchGFWList.Job
+{
+    public class FetchGFWJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            var dir = Path.Combine(System.Web.Hosting.HostingEnvironment.MapPath("/"), "f");
+
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create("https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt");
+                var res = (HttpWebResponse)req.GetResponse();
+                var stream = res.GetResponseStream();
+                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+
+                var content = reader.ReadToEnd();
+
+                if (!Directory.Exists(dir))
+                    Directory.CreateDirectory(dir);
+
+                var mainFile = Path.Combine(dir, "gfw.txt");
+                File.WriteAllText(mainFile, content);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+    }
+
+}
---
+++ FetchGFWList/Job/JobScheduler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Quartz;
+using Quartz.Impl;
+
+namespace FetchGFWList.Job
+{
+    public class JobScheduler
+    {
+        public static void Start()
+        {
+            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+
+            var trigger = TriggerBuilder.Create()
+                    .StartNow()
+                    .WithSimpleSchedule(s => s.WithIntervalInMinutes(5).RepeatForever())
+                    .Build();
+
+            scheduler.ScheduleJob(JobBuilder.Create<FetchGFWJob>().Build(), trigger);
+
+        }
+    }
+}
